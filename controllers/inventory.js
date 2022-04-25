43db452b8fdfad1050976bb71fe8cd5939d9e324
Kaden Hansen <kadendeanhansen@gmail.com>
2022-04-25 18:37:18 -0400

    fix missing comma typo in each router

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -14,29 +14,29 @@ router.get('/:id', async (req, res) => {
 })
 // POST ROUTES
 // add tree card
-router.post('/:id' async (req, res) => {
+router.post('/:id', async (req, res) => {
     //const .... = await ({})
 })
 // add event to tree
-router.post('/:treeid/:eventid' async (req, res) => {
+router.post('/:treeid/:eventid', async (req, res) => {
     //const .... = await ({})
 })
 // EDIT ROUTES
 // edit tree by id
-router.patch('/:treeid' async (req, res) => {
+router.patch('/:treeid', async (req, res) => {
     //const... = await ({})
 })
 // edit event by id
-router.patch('/:treeid/:eventid' async (req, res) => {
+router.patch('/:treeid/:eventid', async (req, res) => {
     //const... = await ({})
 })
 // DELETE ROUTES
 // delete tree by id
-router.delete('/:treeid' async (req,res) => {
+router.delete('/:treeid', async (req,res) => {
     //const.... = await ({})
 })
 // delete event by id
-router.delete('/:treeid/:eventid' async (req, res) => {
+router.delete('/:treeid/:eventid', async (req, res) => {
     //const.... = await ({})
 })
 
