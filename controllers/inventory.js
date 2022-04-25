3c4559d7ba9a9cf310df36f6be8b250a1faaab3c
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-04-25 14:35:18 -0400

    adds rest/crud stubs

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -9,20 +9,34 @@ router.get('/', async (req, res) => {
 })
 
 //  show tree by id
-
+router.get('/:id', async (req, res) => {
+    // const ... = await ...({})
+})
 // POST ROUTES
 // add tree card
-
+router.post('/:id' async (req, res) => {
+    //const .... = await ({})
+})
 // add event to tree
-
+router.post(':eventid' async (req, res) => {
+    //const .... = await ({})
+})
 // EDIT ROUTES
 // edit tree by id
-
+router.patch('treeid' async (req, res) => {
+    //const... = await ({})
+})
 // edit event by id
-
+router.patch('eventid' async (req, res) => {
+    //const... = await ({})
+})
 // DELETE ROUTES
 // delete tree by id
-
+router.delete(':treeid' async (req,res) => {
+    //const.... = await ({})
+})
 // delete event by id
-
+router.delete(':eventid' async (req, res) => {
+    //const.... = await ({})
+})
 module.exports = router
