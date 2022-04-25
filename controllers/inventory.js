c1b14c6f52cb0425536e7de681630105b04dce94
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-04-25 18:29:01 -0400

    adds changes from review

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -18,25 +18,26 @@ router.post('/:id' async (req, res) => {
     //const .... = await ({})
 })
 // add event to tree
-router.post(':eventid' async (req, res) => {
+router.post('/:treeid/:eventid' async (req, res) => {
     //const .... = await ({})
 })
 // EDIT ROUTES
 // edit tree by id
-router.patch('treeid' async (req, res) => {
+router.patch('/:treeid' async (req, res) => {
     //const... = await ({})
 })
 // edit event by id
-router.patch('eventid' async (req, res) => {
+router.patch('/:treeid/:eventid' async (req, res) => {
     //const... = await ({})
 })
 // DELETE ROUTES
 // delete tree by id
-router.delete(':treeid' async (req,res) => {
+router.delete('/:treeid' async (req,res) => {
     //const.... = await ({})
 })
 // delete event by id
-router.delete(':eventid' async (req, res) => {
+router.delete('/:treeid/:eventid' async (req, res) => {
     //const.... = await ({})
 })
+
 module.exports = router
