699b2c0be4a449b955f325769f8687ceb0c0ca27
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-04-25 21:46:25 -0400

    adds remaining CRUD routes

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -10,34 +10,80 @@ router.get('/', async (req, res) => {
 
 //  show tree by id
 router.get('/:id', async (req, res) => {
-    // const ... = await ...({})
+    const trees = await db.Tree.findById({})
+    res.json(trees)
 })
 // POST ROUTES
 // add tree card
 router.post('/:id', async (req, res) => {
-    //const .... = await ({})
+    const treeCard = new TreeCard(req.body);
+    try {
+        await treeCard.save();
+        res.status(201).send(treeCard)
+    } catch (error) {
+        req.status(500).send(error)
+    }
 })
 // add event to tree
 router.post('/:treeid/:eventid', async (req, res) => {
-    //const .... = await ({})
+    const newEvent =  new Event(req.body)
+    try {
+        await newEvent.save()
+        res.status(201).send(newEvent)
+    } catch (error) {
+        req.status(500).send(error)
+    }
 })
 // EDIT ROUTES
 // edit tree by id
-router.patch('/:treeid', async (req, res) => {
-    //const... = await ({})
+router.patch('/edit/:treeid', async (req, res) => {
+    try {
+        const tree = await db.Tree.findByIdAndUpdate(req.params.treeid, req.body, {new: true})
+        if(!tree){
+            return res.status(404)
+        }
+        res.status(200).send(tree)
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 // edit event by id
-router.patch('/:treeid/:eventid', async (req, res) => {
-    //const... = await ({})
+router.patch('/edit/:treeid/:eventid', async (req, res) => {
+    try {
+        const event = await Event.findByIdAndUpdate(req.params.eventid, req.body, { new: true })
+        if (!event) {
+            return res.status(404)
+        }
+        res.status(200).send(event)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
 })
 // DELETE ROUTES
 // delete tree by id
-router.delete('/:treeid', async (req,res) => {
-    //const.... = await ({})
+router.delete('/delete-tree/:treeid', async (req, res) => {
+    try {
+        const tree = await db.Tree.findByIdAndDelete(req.params.id);
+        if (!tree) {
+            return res.status(404).send();
+        }
+        res.send(tree);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 })
 // delete event by id
-router.delete('/:treeid/:eventid', async (req, res) => {
-    //const.... = await ({})
+router.delete('/delete-tree/:treeid/:eventid', async (req, res) => {
+    try {
+        const event = await db.Event.findByIdAndDelete(req.params.id)
+        if (!event) {
+            return res.status(404).send()
+        }
+        res.send(event)
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 module.exports = router
